052c8a2d63e3baf85c00c3addafce0b40aa8c6c5
maria luiza <151561030+maluviieira@users.noreply.github.com>
2025-10-29 11:32:35 +0000

    file name handled in createControlPacket

--- src/application_layer.c
+++ src/application_layer.c
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #define PACKET_SIZE 5 
 
 #define FILE_SIZE 0
 #define FILE_NAME 1
 
-int createControlPacket(unsigned char *packet, const char *filename, long file_size, unsigned char control) {
+int createControlPacket(unsigned char *packet, const char *filename, long file_size, int control) {
     int pos = 0;
    
     packet[pos++] = control;
@@ -22,6 +23,12 @@ int createControlPacket(unsigned char *packet, const char *filename, long file_s
     packet[pos++] = (file_size >> 16) & 0xFF;
     packet[pos++] = (file_size >> 8) & 0xFF;
     packet[pos++] = file_size & 0xFF;
+
+    uint8_t nameLen = strlen(filename);
+    packet[pos++] = FILE_NAME;  // T
+    packet[pos++] = nameLen;    // L
+    memcpy(&packet[pos], filename, nameLen);
+    pos += nameLen;
     
     return pos; // return packet size
 }
@@ -41,6 +48,50 @@ int createDataPacket(unsigned char *packet, const unsigned char *data, int data_
     
     return pos; 
 }
+
+int parseControlPacket(const unsigned char *packet, int packet_size, 
+                        int *control_type, long *file_size, char *filename) {
+    if (packet_size < 7) return -1; // minimum size for start/end
+    
+    *control_type = packet[0];
+    int pos = 1;
+    
+    // reset outputs
+    *file_size = 0;
+    if (filename) filename[0] = '\0';
+    
+    // parse tlv fields
+    while (pos < packet_size) {
+        unsigned char type = packet[pos++];
+        if (pos >= packet_size) return -1;
+        
+        unsigned char length = packet[pos++];
+        if (pos + length > packet_size) return -1;
+        
+        switch (type) {
+            case FILE_SIZE:
+                if (length == 4) {
+                    *file_size = ((long)packet[pos] << 24) | 
+                                ((long)packet[pos+1] << 16) |
+                                ((long)packet[pos+2] << 8) | 
+                                (long)packet[pos+3];
+                }
+                break;
+                
+            case FILE_NAME:
+                if (filename && length > 0) {
+                    memcpy(filename, &packet[pos], length);
+                    filename[length] = '\0';
+                }
+                break;
+        }
+        
+        pos += length;
+    }
+    
+    return 0; // success
+}
+
 
 
 
